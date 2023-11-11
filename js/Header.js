902b89b9aab3673b3d3e5e0f4ad87c99a3425682
Genesis <genesissaldanavasquez@gmail.com>
2023-11-11 18:58:18 -0500

    Mobile finalizado, palabras claves, alt y más

--- js/Header.js
+++ js/Header.js
@@ -2,3 +2,11 @@ window.addEventListener("scroll", function() {
     const header = this.document.querySelector(".header")
     header.classList.toggle("active", window.scrollY > 90)
 })
+
+// Mobile Responsive
+const toggleMenuElement = document.getElementById('toggle-menu');
+const mainMenuElement = document.getElementById('main-menu');
+
+toggleMenuElement.addEventListener('click', () => {
+    mainMenuElement.classList.toggle('main-menu--show')
+})
